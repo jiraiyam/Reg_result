31a5eaa7638e44f151599dda18d4b74bdfd669ea
Mahmoud Elshabrawy <132850462+jiraiyam@users.noreply.github.com>
2024-07-01 23:27:07 +0300

    Update ResultRegression.py

--- ResultRegression.py
+++ ResultRegression.py
@@ -7,8 +7,6 @@ import scipy.stats as stats
 from sklearn import metrics
 import plotly.express as px
 from pylab import rcParams
-
-
 rcParams["figure.figsize"]=(30,18)
 plt.rcParams['figure.dpi'] = 300
 plt.rcParams['font.family'] = 'Arial'
@@ -81,7 +79,10 @@ if result_file is not None or prediction_file is not None:
                 if i * 3 + j < num_metrics:
                     metric = result.columns[i * 3 + j]
                     stats.probplot(result[metric], dist="norm", plot=axes[i, j])
-                    axes[i, j].set_title(f"Q-Q Plot for {metric}")
+                    axes[i, j].set_title(f"Q-Q Plot for {metric}", weight='bold')  # Title in bold
+
+                    axes[i, j].set_xlabel(f"{metric}", weight='bold')  # X-axis label in bold
+                    axes[i, j].set_ylabel("Probability", weight='bold')  # Y-axis label in bold
 
         fig.tight_layout()
         st.pyplot(fig)
@@ -89,20 +90,23 @@ if result_file is not None or prediction_file is not None:
     def plot_heatmap(result):
         st.subheader("Performance Metric Comparison (Heatmap)")
         fig, ax = plt.subplots(figsize=(8, 6))
+        result.drop(['RRMSE'] , axis=1  , inplace=True)
+
         sns.heatmap(result, annot=True, fmt=".2f", cmap="YlGnBu", ax=ax)
         st.pyplot(fig)
 
     def plot_mean_performance_metrics(result):
         st.subheader("Mean Performance Metrics with Error Bars")
+        result.drop(['RRMSE'] , axis=1  , inplace=True)
         mean_metrics = result.mean()
         std_metrics = result.std()
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.bar(mean_metrics.index, mean_metrics, yerr=std_metrics, capsize=5)
         ax.set_xlabel('Metrics')
         ax.set_ylabel('Metric Values')
-        ax.set_title('Mean Performance Metrics with Error Bars')
+        ax.set_title('Mean Performance Metrics with Error Bars' , weight='bold')
         st.pyplot(fig)
-
+##################################################################################################
     def plot_radar_plot(result):
         st.subheader("Radar Plot of Performance Metrics")
         normalized_metrics = (result - result.min()) / (result.max() - result.min())
@@ -113,7 +117,7 @@ if result_file is not None or prediction_file is not None:
             angles = [n / float(len(result.columns)) * 2 * np.pi for n in range(len(result.columns))]
             angles += angles[:1]
             ax.plot(angles, values, label=model)
-        ax.set_title('Radar Plot of Performance Metrics')
+        ax.set_title('Radar Plot of Performance Metrics' , weight='bold')
         ax.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
         st.pyplot(fig)
 
@@ -129,26 +133,46 @@ if result_file is not None or prediction_file is not None:
         ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.9), title='Models', fontsize=8)
         st.pyplot(fig)
 
+
     def plot_scatter_matrix(result):
         st.subheader("Scatter Matrix Plot of Model Comparison")
-        fig, ax = plt.subplots(figsize=(12, 12))
-        pd.plotting.scatter_matrix(result, alpha=0.8, ax=ax, diagonal='hist')
-        fig.suptitle('Scatter Matrix Plot of Model Comparison', y=1.02)
+        fig, axes = plt.subplots(figsize=(12, 12))
+
+        scatter_matrix = pd.plotting.scatter_matrix(result, alpha=0.8, diagonal='hist', ax=axes)
+
+        font = {'weight': 'bold'}
+
+        for ax in scatter_matrix.ravel():
+            ax.set_xlabel(ax.get_xlabel(), fontdict=font)
+            ax.set_ylabel(ax.get_ylabel(), fontdict=font)
+            ax.title.set_fontweight('bold')
+
+        fig.suptitle('Scatter Matrix Plot of Model Comparison', y=1.02, weight='bold')
         fig.tight_layout(h_pad=0.5, w_pad=0.5)
         st.pyplot(fig)
 
+
     def plot_scatter_plot(prediction):
         st.subheader("Scatter Plot of Model Predictions vs. Original Values")
-        fig, ax = plt.subplots(figsize=(12, 8))
+        fig, ax = plt.subplots(figsize=(20, 12))
+
         for model in prediction.columns:
             if model != 'Original':
                 ax.scatter(prediction['Original'], prediction[model], label=model)
-        ax.set_xlabel('Original Values')
-        ax.set_ylabel('Predicted Values')
-        ax.set_title('Scatter Plot of Model Predictions vs. Original Values')
-        ax.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+
+        # Set the font properties
+        font = {'weight': 'bold'}
+
+        # Update the font properties for the axis labels and title
+        ax.set_xlabel('Original Values', fontdict=font)
+        ax.set_ylabel('Predicted Values', fontdict=font)
+        ax.set_title('Scatter Plot of Model Predictions vs. Original Values', fontdict=font)
+
+        ax.legend(loc='upper left', bbox_to_anchor=(1.1, 1.1))
         ax.grid()
+
         st.pyplot(fig)
+
 
     def plot_box_plot(prediction):
         st.subheader("Box Plot of Prediction Errors for Each Model")
@@ -156,8 +180,8 @@ if result_file is not None or prediction_file is not None:
         prediction_errors = prediction.drop(columns='Original').subtract(prediction['Original'], axis=0)
         prediction_errors.boxplot(ax=ax)
         ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-        ax.set_ylabel('Prediction Errors')
-        ax.set_title('Box Plot of Prediction Errors for Each Model')
+        ax.set_ylabel('Prediction Errors' , weight='bold')
+        ax.set_title('Box Plot of Prediction Errors for Each Model' , weight='bold')
         ax.grid()
         st.pyplot(fig)
 
@@ -168,8 +192,8 @@ if result_file is not None or prediction_file is not None:
         prediction_errors = prediction_errors.melt(var_name='Model', value_name='Error')
         sns.violinplot(data=prediction_errors, x='Model', y='Error', ax=ax)
         ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-        ax.set_ylabel('Prediction Errors')
-        ax.set_title('Violin Plot of Prediction Errors for Each Model')
+        ax.set_ylabel('Prediction Errors' , weight='bold')
+        ax.set_title('Violin Plot of Prediction Errors for Each Model' , weight='bold')
         ax.grid()
         st.pyplot(fig)
 
@@ -181,10 +205,10 @@ if result_file is not None or prediction_file is not None:
                 residuals = prediction[model] - prediction['Original']
                 ax.plot(prediction.index, residuals, label=model)
         ax.axhline(y=0, color='r', linestyle='--')
-        ax.set_xlabel('Data Points')
-        ax.set_ylabel('Residuals')
-        ax.set_title('Residual Plot of Model Predictions')
-        ax.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+        ax.set_xlabel('Data Points' , weight='bold')
+        ax.set_ylabel('Residuals' , weight='bold')
+        ax.set_title('Residual Plot of Model Predictions'  , weight='bold')
+        ax.legend(loc='upper left', bbox_to_anchor=(1.1, 1.1))
         ax.grid()
         st.pyplot(fig)
 
@@ -192,9 +216,9 @@ if result_file is not None or prediction_file is not None:
         st.subheader("Mean Squared Error (MSE) Comparison")
         fig, ax = plt.subplots(figsize=(12, 8))
         sns.barplot(x=result.mean(axis=1), y=result.index, palette='viridis', ax=ax)
-        ax.set_title('Mean Squared Error (MSE) Comparison')
-        ax.set_xlabel('Mean Squared Error (MSE)')
-        ax.set_ylabel('Models')
+        ax.set_title('Mean Squared Error (MSE) Comparison' ,weight='bold')
+        ax.set_xlabel('Mean Squared Error (MSE)' , weight='bold')
+        ax.set_ylabel('Models' , weight='bold')
         st.pyplot(fig)
 
     def plot_parallel_coordinates_plotly(result):
@@ -212,15 +236,15 @@ if result_file is not None or prediction_file is not None:
 
     def plot_correlation_heatmap(prediction):
         st.subheader("Correlation Heatmap of Predicted Values by Model")
-        fig, ax = plt.subplots(figsize=(12, 8))
+        fig, ax = plt.subplots(figsize=(20, 12))
         sns.heatmap(prediction.corr(), annot=True, cmap='coolwarm', fmt='.2f', ax=ax)
-        ax.set_title('Correlation Heatmap of Predicted Values by Model')
+        ax.set_title('Correlation Heatmap of Predicted Values by Model', weight='bold')
         st.pyplot(fig)
 
 
     def plot_r2_polar_plot(prediction):
         st.subheader("R2 Polar Plot")
-        fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={'projection': 'polar'})
+        fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': 'polar'})
 
         for model in prediction.columns[:-1]:
             mse = metrics.mean_squared_error(prediction[prediction.columns[-1]], prediction[model])
@@ -229,31 +253,57 @@ if result_file is not None or prediction_file is not None:
 
             ax.scatter(np.arccos(r2), np.sqrt(mse), s=30, label=model)
 
-        ax.set_title("R2 Polar Plot")
+        ax.set_title("R2 Polar Plot", weight='bold')
         ax.legend(loc='upper left', bbox_to_anchor=(1.1, 1.1))  # Place legend outside the plot
         st.pyplot(fig)
+
+
     def plot_pairplot(result):
         st.subheader("Pairplot with Hue")
-        fig = sns.pairplot(result,  palette='viridis')
-        st.pyplot(fig)
+        pairplot = sns.pairplot(result, palette='viridis')
+        font = {'weight': 'bold'}
+        for ax in pairplot.axes.flatten():
+            ax.set_xlabel(ax.get_xlabel(), fontdict=font)
+            ax.set_ylabel(ax.get_ylabel(), fontdict=font)
+            ax.title.set_fontweight('bold')
+
+        st.pyplot(pairplot)
 
 
     def plot_distplot(result):
         st.subheader("Distribution Plot for Each Metric")
         fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(18, 18))
         metrics = result.columns
+
+        # Set the font properties
+        font = {'weight': 'bold'}
+
         for i, metric in enumerate(metrics):
             row = i // 3
             col = i % 3
             sns.histplot(result[metric], kde=True, ax=axes[row, col])
-            axes[row, col].set_title(f"Distribution of {metric}")
+            axes[row, col].set_title(f"Distribution of {metric}", fontdict=font)
+            axes[row, col].set_xlabel(metric, fontdict=font)
+            axes[row, col].set_ylabel('Frequency', fontdict=font)
+
         fig.tight_layout()
         st.pyplot(fig)
 
 
     def plot_jointplot(prediction):
         st.subheader("Jointplot of Model Predictions vs. Original Values")
+
         fig = sns.jointplot(x='Original', y=prediction.columns[1], data=prediction, kind='reg', height=8)
+
+        font = {'weight': 'bold'}
+
+        fig.ax_joint.set_xlabel('Original Values', fontdict=font)
+        fig.ax_joint.set_ylabel('Predicted Values', fontdict=font)
+        fig.fig.suptitle("Jointplot of Model Predictions vs. Original Values", fontdict=font)
+
+        fig.fig.tight_layout()
+        fig.fig.subplots_adjust(top=0.95)
+
         st.pyplot(fig)
 
 
@@ -274,31 +324,76 @@ if result_file is not None or prediction_file is not None:
 
     def plot_histogram_kde(result):
         st.subheader("Histogram with Marginal KDE")
+
         fig, axes = plt.subplots(nrows=len(result.columns), ncols=len(result.columns))
+
+        font = {'weight': 'bold'}
+
         for i, col1 in enumerate(result.columns):
             for j, col2 in enumerate(result.columns):
                 if i == j:
                     sns.histplot(result[col1], kde=True, ax=axes[i, j])
                 else:
                     sns.scatterplot(x=result[col1], y=result[col2], ax=axes[i, j])
+
+                if i == len(result.columns) - 1:
+                    axes[i, j].set_xlabel(col2, fontdict=font)
+                if j == 0:
+                    axes[i, j].set_ylabel(col1, fontdict=font)
+
+                axes[i, j].set_title(f"{col1} vs {col2}" if i != j else f"Distribution of {col1}", fontdict=font)
+
         fig.tight_layout()
         st.pyplot(fig)
 
 
     def plot_pairgrid(result):
         st.subheader("PairGrid with Regression Fits")
+
+        # Create the PairGrid
         g = sns.PairGrid(result)
         g.map_upper(sns.scatterplot)
         g.map_lower(sns.kdeplot, cmap="Blues_d")
         g.map_diag(sns.histplot, kde=True)
         g.map_lower(sns.regplot)
+
+        # Set the font properties
+        font = {'weight': 'bold'}
+
+        # Update the font properties for axis labels and titles
+        for ax in g.axes.flatten():
+            ax.set_title(ax.get_title(), fontdict=font ,weight='bold')
+            ax.set_xlabel(ax.get_xlabel(), fontdict=font ,weight='bold')
+            ax.set_ylabel(ax.get_ylabel(), fontdict=font , weight='bold')
+
+        g.fig.suptitle("PairGrid with Regression Fits", fontdict=font, y=1.05, weight='bold')
+
+
         st.pyplot(g)
 
 
     def plot_clustermap(result):
         st.subheader("Clustermap for Metric Correlation")
+
+        # Compute the correlation matrix
         corr = result.corr()
+
+        # Create the clustermap
         g = sns.clustermap(corr, cmap='coolwarm', linewidths=.5)
+
+        # Set the font properties
+        font = {'weight': 'bold'}
+
+        # Update the font properties for axis labels
+        for label in g.ax_heatmap.get_xticklabels():
+            label.set_fontweight('bold')
+        for label in g.ax_heatmap.get_yticklabels():
+            label.set_fontweight('bold')
+
+        # Add a title
+        g.fig.suptitle("Clustermap for Metric Correlation", fontdict=font,weight='bold')
+
+
         st.pyplot(g)
 
 
@@ -309,17 +404,34 @@ if result_file is not None or prediction_file is not None:
         std_metrics = result.std()
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.errorbar(mean_metrics.index, mean_metrics, yerr=std_metrics, fmt='o', capsize=5)
-        ax.set_xlabel('Metrics')
-        ax.set_ylabel('Metric Values')
-        ax.set_title('Mean Performance Metrics with Error Bars')
+        ax.set_xlabel('Metrics',weight='bold')
+        ax.set_ylabel('Metric Values',weight='bold')
+        ax.set_title('Mean Performance Metrics with Error Bars' , weight='bold')
         ax.grid(True)
         st.pyplot(fig)
 
 
     def plot_scatter_matrix_regression(result):
         st.subheader("Scatter Plot Matrix with Regression Fits")
+
+        # Create the scatter plot matrix with regression fits
         fig = sns.pairplot(result, kind="reg", plot_kws={'line_kws': {'color': 'red'}})
+
+        # Set the font properties
+        font = {'weight': 'bold'}
+
+        # Update the font properties for axis labels and title
+        for ax in fig.axes.flatten():
+            ax.set_xlabel(ax.get_xlabel(), fontdict=font , weight='bold')
+            ax.set_ylabel(ax.get_ylabel(), fontdict=font , weight='bold')
+            ax.set_title(ax.get_title(), fontdict=font ,  weight='bold')
+
+        fig.fig.suptitle("Scatter Plot Matrix with Regression Fits", fontdict=font, y=1.02 , weight='bold')
+        fig.fig.tight_layout()
+
         st.pyplot(fig)
+
+
 
 
     # Choose the plot based on the user's selection
